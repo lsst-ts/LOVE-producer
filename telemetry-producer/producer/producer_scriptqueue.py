d386460d5ee5b7d75540d6dc491ac9ed51c6e6f8
José  Galaz <jdgalazm@gmail.com>
2019-03-08 16:18:17 -0300

    create message from state to be sent with ws

--- telemetry-producer/producer/producer_scriptqueue.py
+++ telemetry-producer/producer/producer_scriptqueue.py
@@ -56,55 +56,98 @@ class ScriptQueueProducer:
     def monitor_script(self, salindex):
         self.queue.get_script_remote(salindex)
 
-        t = threading.Thread(target=self.queue.monitor_script, args=[salindex])
-        t.start()
+        # print('salindex:', salindex, 'remote:', self.queue.state.scripts[self.salindex]['remote'])
+
+        # t = threading.Thread(target=self.queue.monitor_script(salindex), args=[salindex])
+        # t.start()
     
-    def send_ws_data(self, ws):
-        """
-            Prepares a message to be sent with websockets (for example),
-            based on the updated state of the queue.
-        """
-
-        indices = list(self.queue.state.scripts.keys())
-
-        if len(indices) == 0: 
-            print('no data')
-            return
-
+    def update(self):
         self.queue.update_scripts_info()
         self.queue.update_queue_state()
 
-        message = {
-          'script_queue_state': {
-            'waiting_script_indices': self.queue.state._queue_script_indices,
-            'finished_script_indices': self.queue.state._past_script_indices,
-            'current_script_index': self.queue.state._current_script_index,
-            'state': self.queue.state.state
-            'scripts': {},
-          }
-        }
-        
-        print('\n\n\n\t current', self.queue.state._current_script_index)
-        print('\t waiting:', self.queue.state._queue_script_indices)
-        print('\t finished', self.queue.state._past_script_indices)
-        import pprint
-        
-        
-        for salindex in indices:
-            info = {**self.queue.state.scripts[salindex]}
-            info['script_state'] = info['script_state'].name
-            info['process_state'] = info['process_state'].name
-            info['elapsed_time'] = info['duration']
-            del info['duration']
-            message['script_queue_state']['scripts'][salindex] = info
-        
-        return message
+    def parse_script(script):
+        new_script = {**script}
+        new_script['script_state'] = new_script['script_state'].name
+        new_script['process_state'] = new_script['process_state'].name
+        new_script['elapsed_time'] = new_script['duration']; 
+        discard = ['duration', 'remote', 'updated']
+        for name in discard:
+            del new_script[name]
+        return new_script
 
-        # # print(10*'\n','duration', self.queue.state.scripts[salindex]['duration'])
-        # if self.queue.state.scripts[salindex]['process_state'] >= ScriptProcessState.DONE:
-        #     print('done')
-        #     return
-        # remote = self.queue.state.scripts[salindex]['remote']        
-        # values = get_remote_event_values(remote)
-        # print(5*'\n', self.queue.state.scripts[salindex]['process_state'])
-        # print(values)
+    def parse_queue_state(self):
+        """
+            Gets the updated state and returns it in a LOVE-friendly format
+        """
+
+        # "get_queue_state" makes the update before parsing the state
+        queue_state = self.queue.get_queue_state() 
+        queue_state['finished_scripts'] = queue_state['past_scripts']
+        queue_state['waiting_scripts'] = queue_state['queue_scripts']
+        del queue_state['past_scripts']
+        del queue_state['queue_scripts']
+
+        for queue_name in ['waiting_scripts', 'finished_scripts']:
+            queue = queue_state[queue_name]
+            for script in list(queue.keys()):
+                queue[script] = self.parse_script(queue[script])
+
+        if queue_state['current'] == None:
+            queue_state['current'] = 'None'
+        else:
+            queue_state['current'] = self.parse_script(queue_state['current'])
+            
+        return queue_state
+    # def send_ws_data(self, ws):
+    #     """
+    #         Prepares a message to be sent with websockets (for example),
+    #         based on the updated state of the queue.
+    #     """
+
+    #     indices = list(self.queue.state.scripts.keys())
+
+    #     if len(indices) == 0: 
+    #         print('no data')
+    #         return
+
+    #     self.update()
+
+
+    #     queue_state = self.queue.get_queue_state()
+    #     message = {
+    #       'script_queue_state': {
+    #         'waiting_scripts': queue_state['queue_scripts'],
+    #         'finished_scripts': queue_state['past_scripts'],
+    #         'current_script_index': queue_state['current'],
+    #         'state': queue_state['state'],
+    #         'scripts': {},
+    #       }
+    #     }
+        
+    #     print('\n\n\n\t current', self.queue.state._current_script_index)
+    #     print('\t waiting:', self.queue.state._queue_script_indices)
+    #     print('\t finished', self.queue.state._past_script_indices)
+    #     import pprint
+        
+
+
+    #     for salindex in indices:
+    #         info = {**self.queue.state.scripts[salindex]}
+    #         info['script_state'] = info['script_state'].name
+    #         info['process_state'] = info['process_state'].name
+    #         info['elapsed_time'] = info['duration']; del info['duration']
+            
+    #         message['script_queue_state']['scripts'][salindex] = info
+        
+    #         # remote = self.queue.state.scripts[salindex]['remote']        
+    #         # print(remote)
+    #         # get_remote_event_values(remote)
+    #     return message
+
+    #     # # print(10*'\n','duration', self.queue.state.scripts[salindex]['duration'])
+    #     # if self.queue.state.scripts[salindex]['process_state'] >= ScriptProcessState.DONE:
+    #     #     print('done')
+    #     #     return
+    #     # values = get_remote_event_values(remote)
+    #     # print(5*'\n', self.queue.state.scripts[salindex]['process_state'])
+    #     # print(values)
--- telemetry-producer/producer/test_producer_scriptqueue.py
+++ telemetry-producer/producer/test_producer_scriptqueue.py
@@ -1,12 +1,93 @@
-from producer_scriptqueue import ScriptQueueProducer
+from producer_scriptqueue import ScriptQueueProducer, get_remote_event_values
+from lsst.ts import salobj
+import SALPY_Script
+import pprint
+import time
 # from importlib import reload
 # reload(producer_scriptqueue)
+
+# logfile = open('log.txtadsf','w')
+
 sqp = ScriptQueueProducer()
-# sqp.do_run()
-import time
+
 while True:
-    sqp.send_ws_data(1)
-    time.sleep(2)
+    message = sqp.parse_queue_state()
+    pprint.pprint(message)
+    time.sleep(1)
+# while True:
+#     try:
+#         state = sqp.queue.get_queue_state()
+#         # pprint.pprint(state)
+#         for queue in ['past_scripts', 'queue_scripts']:
+#             if state[queue] is None:
+#                 continue
+#             script = parseScript(queue[100000])
+#             print(queue, script[queue])
+#             # for script in state[queue].keys():
+#             #     pprint.pprint(queue, script)
+#     except:
+#         print('error')
+#     time.sleep(1)
+
+
+# sqp.do_run()
+# import time
+# while True:
+#     sqp.send_ws_data(1)
+#     # time.sleep(2)
+#     break
+
+# script_indices = []
+# remotes = {}
+
+# # while True:
+# print('-')
+# for salindex in list(sqp.queue.state.scripts.keys()):
+#     remotes[salindex] = sqp.queue.state.scripts[salindex]['remote']
+#     print(salindex, remotes[salindex])
+
+# sqp.queue.update_queue_state
+
+
+
+# for salindex in script_indices:
+#     values = get_remote_event_values(remotes[salindex])
+#     if any(values): 
+#         print(values)
+#         break
+# time.sleep(1)
+# i = 0
+# while True:
+#     i+=1
+
+#     if queue.state.scripts[salindex]['process_state'] >= ScriptProcessState.DONE:
+#         print('done')
+#         break
+#     values = get_remote_event_values(remote)
+#     print(5*'\n',i,queue.state.scripts[salindex]['process_state'])
+#     print(values)
+#     time.sleep(1.0)
+    
+
+# get script remote        
+# sqp.queue.get_script_remote(salindex)
+# remote = queue.state.scripts[salindex]['remote']
+
+# queue.state.scripts[100020]['duration']
+
+
+# import threading
+
+# t = threading.Thread(target=queue.monitor_script, args=[salindex])
+# t.start()
+# print('\n\n\nprint print')
+# # loop = asyncio.get_event_loop()
+# # loop.run_until_complete(printstuff(salindex))
+
+# print(dir(remote))
+
+
+
 
 # salindex = 100005
 # self.queue.state.update_script_info(self.queue.queue.evt_script.get())
@@ -18,4 +99,23 @@ while True:
 # from lsst.ts.scriptqueue.base_script import ScriptState
 
 # statesDict = { getattr(ScriptState, state).name: getattr(ScriptState, state).value for state in dir(ScriptState) if not state.startswith('__')}
+
+
+# sqp.queue.get_script_remote(salindex)
+# remote = queue.state.scripts[salindex]['remote']
+
+
+# salindex = 100013
+# sqp.monitor_script(100015)
+# import asyncio
+
+# async def runit():
+#     await sqp.queue.evt_loop.run_until_complete(sqp.monitor_script(100018))
+
+# sqp.monitor_script(100019)
+# sqp.queue.evt_loop.run_until_complete(runit())
+# sqp.queue.state.scripts[salindex]['remote'] = salobj.Remote(SALPY_Script, salindex)
+# remote = salobj.Remote(SALPY_Script, salindex)
+
+# >>> remote.evt_metadata.get_oldest() da none por algun motivo
 
