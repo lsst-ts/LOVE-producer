e4bc4f085e6312e562ad5ce45a17000dff8a88b5
José  Galaz <jdgalazm@gmail.com>
2019-03-26 11:48:33 -0300

    dont start queue in start-daemon.sh

--- telemetry-producer/producer/start-daemon.sh
+++ telemetry-producer/producer/start-daemon.sh
@@ -2,6 +2,4 @@
 
 source /home/saluser/.setup.sh
 
-run_script_queue.py 1 ~/repos/ts_scriptqueue/tests/data/standard/ ~/repos/ts_scriptqueue/tests/data/external/ &
-
 python -u /usr/src/love/main.py
