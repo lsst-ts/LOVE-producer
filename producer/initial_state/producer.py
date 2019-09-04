e04e58ec5fd3e2262b8e952d1982d43c8588d342
José  Galaz <jdgalazm@gmail.com>
2019-09-04 18:43:02 -0400

    pass test with initial_state producer

--- producer/initial_state/producer.py
+++ producer/initial_state/producer.py
@@ -7,17 +7,38 @@ from lsst.ts import salobj
 
 class InitialStateProducer:
     def __init__(self, domain, csc_list):
-        pass
+        self.remote_dict = {}
+        for (csc, salindex) in csc_list:
+            try:
+                self.remote_dict[(csc, salindex)] = salobj.Remote(domain, csc, salindex)
+            except Exception as e:
+                print('InitialStateProducer could not load Remote for', csc, salindex)
 
     async def process_message(self, message):
         """ Tries to obtain the current data for an event with salobj """
+        request_data = message["data"][0]
+        csc = request_data["csc"]
+        salindex = request_data["salindex"]
+        event_name = request_data["data"]["stream"]["event_name"]
+
+        remote = self.remote_dict[(csc, salindex)]
+        evt_object = getattr(remote, "evt_{}".format(event_name))
+        evt_data = await evt_object.next(flush=False)
+        result = {}
+        for parameter_name in evt_data._member_attributes:
+            result[parameter_name] = getattr(evt_data, parameter_name)
+            parameter_data = getattr(evt_data, parameter_name)
+            result[parameter_name] = {
+                'value': parameter_data,
+                'dataType': getDataType(parameter_data)
+            }
 
         message = {
             "category": "event",
             "data": [{
-                "csc": '',
-                "salindex": 0,
-                "data": {"summaryState": []}
+                "csc": csc,
+                "salindex": salindex,
+                "data": {"summaryState": [result]}
             }]
         }
         return message
