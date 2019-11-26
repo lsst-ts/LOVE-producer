8b689827d031f27bfeb152991905652a3c63e26d
José  Galaz <jdgalazm@gmail.com>
2019-11-26 13:35:46 -0300

    refactor command receiver test

--- producer/command_receiver/test.py
+++ producer/command_receiver/test.py
@@ -3,73 +3,46 @@ from .receiver import Receiver
 import asyncio
 from lsst.ts import salobj
 import json
-import unittest
-from utils import NumpyEncoder
-
-STD_TIMEOUT = 5  # timeout for command ack
-SHOW_LOG_MESSAGES = False
+import asynctest
+import utils
 
 index_gen = salobj.index_generator()
 
 
-class Harness:
-    def __init__(self, initial_state, config_dir=None, CscClass=salobj.TestCsc):
-        index = next(index_gen)
-        self.csc = CscClass(index=index, config_dir=config_dir, initial_state=initial_state)
-        if SHOW_LOG_MESSAGES:
-            handler = logging.StreamHandler()
-            self.csc.log.addHandler(handler)
-        self.remote = salobj.Remote(domain=self.csc.domain, name="Test", index=index)
-
-    async def __aenter__(self):
-        await self.csc.start_task
-        await self.remote.start_task
-        return self
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self.remote.close()
-        await self.csc.close()
-
-
-class TestCommandsFromMessages(unittest.TestCase):
-    def setUp(self):
+class TestCommandsFromMessages(asynctest.TestCase):
+    async def test_command_ack(self):
         salobj.set_random_lsst_dds_domain()
-        self.maxDiff = None
+        index = next(index_gen)
+        csc = salobj.TestCsc(index=index, config_dir=None, initial_state=salobj.State.ENABLED)
+        remote = salobj.Remote(domain=csc.domain, name="Test", index=index)
+        await asyncio.gather(csc.start_task, remote.start_task)
 
-    def test_command_ack(self):
-        async def doit():
-            async with Harness(initial_state=salobj.State.ENABLED) as harness:
-                # Arrange
-                cmd_data_sent = harness.csc.make_random_cmd_scalars()
-                cmd_receiver = Receiver(domain=harness.csc.domain, csc_list=[('Test', harness.csc.salinfo.index)])
-                cmd_message = {
-                    "category": "cmd",
-                    "data": [
-                        {
-                            "csc": "Test",
-                            "salindex": harness.csc.salinfo.index,
-                            "data": {
-                                "stream": {
-                                    "cmd": "cmd_setScalars",
-                                    "params": dict(cmd_data_sent.get_vars())
-                                }
-                            }
+        # Arrange
+        cmd_data_sent = csc.make_random_cmd_scalars()
+        cmd_receiver = Receiver(domain=csc.domain, csc_list=[('Test', csc.salinfo.index)])
+        cmd_message = {
+            "category": "cmd",
+            "data": [
+                {
+                    "csc": "Test",
+                    "salindex": csc.salinfo.index,
+                    "data": {
+                        "stream": {
+                            "cmd": "cmd_setScalars",
+                            "params": dict(cmd_data_sent.get_vars())
                         }
-                    ]
+                    }
                 }
+            ]
+        }
 
-                # Act
-                answer = await cmd_receiver.process_message(json.dumps(cmd_message, cls=NumpyEncoder))
+        # Act
+        answer = await cmd_receiver.process_message(cmd_message)
 
-                # Assert
-                expected_answer = cmd_message.copy()
-                expected_answer["category"] = "ack"
-                expected_answer["data"][0]["data"]["stream"]["result"] = "Done"
+        # Assert
+        expected_answer = cmd_message.copy()
+        expected_answer["category"] = "ack"
+        expected_answer["data"][0]["data"]["stream"]["result"] = "Done"
 
-                self.assertEqual(expected_answer, answer)
-
-        asyncio.get_event_loop().run_until_complete(doit())
-
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(expected_answer, answer)
+        await asyncio.gather(remote.close(), csc.close())
