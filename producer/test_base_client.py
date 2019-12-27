479f18fe6b27e6887592cfb976a9961cf2474b2b
José  Galaz <jdgalazm@gmail.com>
2019-12-27 12:32:53 -0300

    mock config file

--- producer/test_base_client.py
+++ producer/test_base_client.py
@@ -2,8 +2,8 @@ import asyncio
 import os
 import websockets
 import asynctest
-from asynctest.mock import patch
-
+from asynctest import mock as am
+import json
 from base_ws_client import BaseWSClient
 
 WS_HOST = "ASDF"
@@ -14,7 +14,29 @@ ENVIRON = {
     "PROCESS_CONNECTION_PASS": WS_PASS
 }
 
-@asynctest.mock.patch.dict(os.environ, ENVIRON)
+CONFIG = json.dumps({
+    "ATMCS": [
+        {"source": "command_sim"}
+    ],
+    "ATDome": [
+        {"source": "command_sim"}
+    ],
+    "Test": [
+        {"index": 1, "source": "command_sim"},
+        {"index": 2, "source": "command_sim"},
+        {"index": 3, "source": "command_sim"}
+    ],
+    "ScriptQueue": [
+        {"index": 1, "source": "command_sim"}
+    ],
+    "Watcher": [
+        {"index": 0, "source": "command_sim"}
+    ]
+})
+
+
+@am.patch.dict(os.environ, ENVIRON)
+@am.patch('builtins.open', am.mock_open(read_data=CONFIG))
 class BaseCientTestCase(asynctest.TestCase):
     async def test_constructor(self):
         client = BaseWSClient('TestClient')
