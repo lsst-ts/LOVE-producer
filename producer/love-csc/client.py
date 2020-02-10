2d7845b1461e30277a1abb36faac5bf3a5ec6b65
José  Galaz <jdgalazm@gmail.com>
2020-02-10 18:18:44 -0300

    add&test love csc client behavior

---
+++ producer/love-csc/client.py
@@ -0,0 +1,53 @@
+import asyncio
+import json
+import websockets
+from lsst.ts import salobj
+from events.producer import EventsProducer
+import os
+import utils
+
+from base_ws_client import BaseWSClient
+from .csc import LOVECsc
+
+
+class LOVEWSClient(BaseWSClient):
+    """Handles the websocket client connection between the Telemetries&Events Producer and the LOVE-manager."""
+
+    def __init__(self):
+        super().__init__(name='Events')
+        self.connection_error = False
+        self.csc = LOVECsc()
+
+    async def on_start_client(self, websocket):
+        """ Initializes the websocket client and producer callbacks """
+        self.connection_error = False
+        self.websocket = websocket
+        await self.csc.start_task
+
+    async def on_websocket_receive(self, websocket, message):
+        if 'category' not in message:
+            return
+        if message['category'] != 'love_csc':
+            return
+
+        if 'data' not in message:
+            return
+        if len(message['data']) == 0:
+            return
+
+        user = utils.get_parameter_from_last_message(message, 'love_csc', 'love', 0, 'logs', 'user' )
+        log_message = utils.get_parameter_from_last_message(message, 'love_csc', 'love', 0, 'logs', 'message' )
+        self.csc.add_observing_log(user, log_message)
+
+    async def on_websocket_error(self, e):
+        self.connection_error = True
+
+async def main():
+    telev_client = LOVEWSClient()
+    await telev_client.start_ws_client()
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.create_task(main())
+    loop.run_forever()
--- producer/love-csc/test.py
+++ producer/love-csc/test.py
@@ -1,8 +1,12 @@
 import asynctest
+from asynctest import mock
+import os
 import asyncio
 from lsst.ts import salobj
 import websockets
 from .csc import LOVECsc
+import utils 
+import json
 
 STD_TIMEOUT = 15  # timeout for command ack
 SHOW_LOG_MESSAGES = False
@@ -20,13 +24,10 @@ class TestLOVECsc(asynctest.TestCase):
         await self.csc.start_task
         await self.remote.start_task
 
-
         # Act: write down some logs and get the results from the event
         self.remote.evt_observingLog.flush()
-        self.csc.add_observing_log('an user','a message')
-        print(result)
-        import pdb; pdb.set_trace()
-        
+        self.csc.add_observing_log('an user', 'a message')
+
         # Assert
         result = await self.remote.evt_observingLog.next(flush=False)
         self.assertEqual(result.user, 'an user')
@@ -36,31 +37,53 @@ class TestLOVECsc(asynctest.TestCase):
         await self.csc.close()
         await self.remote.close()
 
+
+@mock.patch.dict(os.environ, {'WEBSOCKET_HOST': '0.0.0.0:9999', 'PROCESS_CONNECTION_PASS': ''})
 class TestWebsocketsClient(asynctest.TestCase):
+    async def test_csc_client(self):
+        from .client import LOVEWSClient
 
-    async def test_response(self):
-        async def hello_server(websocket, path):
-            name = await websocket.recv()
-            print(f"< {name}")
+        salobj.set_random_lsst_dds_domain()
+        # Arrange
+        test_finished = asyncio.Future()
+        self.remote = salobj.Remote(domain=salobj.Domain(), name="LOVE")
 
-            greeting = f"Hello {name}!"
+        async def server_callback(websocket, path):
+            """Server-ready callback. Runs the test actions and assertions"""
 
-            await websocket.send(greeting)
-            print(f"> {greeting}")    
+            # wait for the client to connect to initial_state group
+            initial_subscription = await websocket.recv()
+
+            # Act
+            # send observing logs form the server (manager)
+            self.remote.evt_observingLog.flush()
+            message = utils.make_stream_message('love_csc','love', 0, 'logs', {
+                'user': 'an user',
+                'message': 'a message'
+            })
+
+            await websocket.send(json.dumps(message))
+
+            # Assert the DDS received the log message
+            result = await self.remote.evt_observingLog.next(flush=False)
+            self.assertEqual(result.user, 'an user')
+            self.assertEqual(result.message, 'a message')
+            test_finished.set_result(True)
+
+
+        # Create server (LOVE-manager mock) and handle it with `server_callback`
+        async with websockets.serve(server_callback, '0.0.0.0', 9999):
+            # connect client (producer)
+            client = LOVEWSClient()
+            client_task = asyncio.create_task(client.start_ws_client())                
             
-        async def hello():
-            uri = "ws://0.0.0.0:9999"
-            async with websockets.connect(uri) as websocket:
-                name = 'name'
+            # Finished resolves after asserts are ok
+            await test_finished
 
-                await websocket.send(name)
-                print(f"> {name}")
-
-                greeting = await websocket.recv()
-                print(f"< {greeting}")
-        
-        
-        async with websockets.serve(hello_server, '0.0.0.0', 9999) as server:
-            await hello()
-        
-        # async wi
+            # cleanup
+            client.retry = False
+            client_task.cancel()
+            await client_task
+            
+            await client.csc.close()
+            await self.remote.close()
