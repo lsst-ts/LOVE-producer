4f1ce7e2fa0196e9451d074f5437dd1a2d477973
José  Galaz <jdgalazm@gmail.com>
2020-02-10 16:26:25 -0300

    create loveCSC with observingLog capabilites

--- producer/love-csc/csc.py
+++ producer/love-csc/csc.py
@@ -13,77 +13,14 @@ HEARTBEAT_INTERVAL = 1  # seconds
 
 
 class LOVECsc(salobj.Controller):
-    """Base class for a Commandable SAL Component (CSC)
-
-    To implement a CSC in Python define a subclass of this class.
-
-    Parameters
-    ----------
-    name : `str`
-        Name of SAL component.
-    index : `int` or `None`
-        SAL component index, or 0 or None if the component is not indexed.
-    initial_state : `State` or `int` (optional)
-        The initial state of the CSC. This is provided for unit testing,
-        as real CSCs should start up in `State.STANDBY`, the default.
-    simulation_mode : `int` (optional)
-        Simulation mode. The default is 0: do not simulate.
-    initial_simulation_mode : `int` (optional)
-        A deprecated synonym for simulation_mode.
-
-    Raises
-    ------
-    ValueError
-        If ``initial_state`` is invalid, or
-        ``simulation_mode`` and ``initial_simulation_mode`` are both nonzero.
-    salobj.ExpectedError
-        If ``simulation_mode`` is invalid.
-        Note: you will only see this error if you await `start_task`.
-
-
-    Notes
-    -----
-    **Attributes**
-
-    * ``heartbeat_interval``: interval between heartbeat events, in seconds;
-      a `float`.
-
-    **Constructor**
-
-    The constructor does the following, beyond the parent class constructor:
-
-    * For each command defined for the component, find a ``do_<name>`` method
-      (which must be present) and add it as a callback to the
-      ``cmd_<name>`` attribute.
-    * The base class provides asynchronous ``do_<name>`` methods for the
-      standard CSC commands. The default implementation is as follows
-      (if any step fails then the remaining steps are not performed):
-
-        * Check for validity of the requested state change;
-          if the change is valid then:
-        * Call ``begin_<name>``. This is a no-op in the base class,
-          and is available for the subclass to override. If this fails,
-          then log an error and acknowledge the command as failed.
-        * Change self.summary_state.
-        * Call ``end_<name>``. Again, this is a no-op in the base class,
-          and is available for the subclass to override.
-          If this fails then revert ``self.summary_state``, log an error,
-          and acknowledge the command as failed.
-        * Call `handle_summary_state` and `report_summary_state`
-          to handle report the new summary state.
-          If this fails then leave the summary state updated
-          (since the new value *may* have been reported),
-          but acknowledge the command as failed.
-        * Acknowledge the command as complete.
-
-    * Set the summary state.
-    * Run `start` asynchronously.
+    """
+        
     """
     def __init__(self):
         super().__init__(name='LOVE', index=None, do_callbacks=False)
-        self.heartbeat_interval = HEARTBEAT_INTERVAL
+    #     self.heartbeat_interval = HEARTBEAT_INTERVAL
 
-    def observing_log(self, user, message):
+    def add_observing_log(self, user, message):
         """Add message to observing log"""
         self.evt_observingLog.set(user=user, message=message)
         self.evt_observingLog.put()
--- producer/love-csc/test.py
+++ producer/love-csc/test.py
@@ -1,9 +1,8 @@
 import asynctest
-import logging
-from .csc import LOVECsc
 import asyncio
 from lsst.ts import salobj
-import utils
+import websockets
+from .csc import LOVECsc
 
 STD_TIMEOUT = 15  # timeout for command ack
 SHOW_LOG_MESSAGES = False
@@ -12,20 +11,27 @@ index_gen = salobj.index_generator()
 
 
 class TestLOVECsc(asynctest.TestCase):
-    async def setUp(self):
+    async def test_add_observing_log(self):
+        """Test that logs work directly from the csc method """
         salobj.set_random_lsst_dds_domain()
+        # Arrange
         self.csc = LOVECsc()
         self.remote = salobj.Remote(domain=self.csc.domain, name="LOVE")
+        await self.csc.start_task
+        await self.remote.start_task
 
-    async def tearDown(self):
-        await asyncio.wait_for(self.csc.close(), STD_TIMEOUT)
-        await asyncio.wait_for(self.remote.close(), STD_TIMEOUT)
 
-    async def test_produced_message_with_event_arrays(self):
-        # Arrange
-        # Setup the producer and the data
-
-        # Act
-
+        # Act: write down some logs and get the results from the event
+        self.remote.evt_observingLog.flush()
+        self.csc.add_observing_log('an user','a message')
+        print(result)
+        import pdb; pdb.set_trace()
+        
         # Assert
-        self.assertEqual(True, True)
+        result = await self.remote.evt_observingLog.next(flush=False)
+        self.assertEqual(result.user, 'an user')
+        self.assertEqual(result.message, 'a message')
+
+        # clean up
+        await self.csc.close()
+        await self.remote.close()
