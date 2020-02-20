4cfb6547e5e5d24d028930db9f0f21ca8c8d92b5
José  Galaz <jdgalazm@gmail.com>
2020-02-20 16:28:18 -0300

    split producer with environment variable

--- producer/main.py
+++ producer/main.py
@@ -5,9 +5,19 @@ from events.client import main as events
 from scriptqueue.client import main as scriptqueue
 from heartbeats.client import main as heartbeats
 from command_receiver.client import main as command_receiver
+import os
 
+TELEMETRIES = "TELEMETRIES"
+EVENTS = "EVENTS"
+CSC_HEARTBEATS = "CSC_HEARTBEATS"
+SCRIPTQUEUE = "SCRIPTQUEUE"
+COMMANDS = "COMMANDS"
 
 if __name__ == '__main__':
+    producers = os.environ.get('LOVE_PRODUCERS').split(':')
+    if len(producers) == 0:
+        producers = [TELEMETRIES, EVENTS, CSC_HEARTBEATS, SCRIPTQUEUE, COMMANDS]
+        
     # with entrypoint(MemoryTracer(interval=10, top_results=10)) as loop:
     def exception_handler(loop, context):
         print("Caught the following exception")
@@ -16,9 +26,19 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.set_exception_handler(exception_handler)
     # loop.set_debug(True)
-    loop.create_task(telemetries())
-    loop.create_task(events())
-    loop.create_task(heartbeats())
-    loop.create_task(scriptqueue())
-    loop.create_task(command_receiver())
+    if TELEMETRIES in producers:
+        print(f'creating {TELEMETRIES} producer')
+        loop.create_task(telemetries())
+    if EVENTS in producers:
+        print(f'creating {EVENTS} producer')
+        loop.create_task(events())
+    if CSC_HEARTBEATS in producers:
+        print(f'creating {CSC_HEARTBEATS} producer')
+        loop.create_task(heartbeats())
+    if SCRIPTQUEUE in producers:
+        print(f'creating {SCRIPTQUEUE} producer')
+        loop.create_task(scriptqueue())
+    if COMMANDS in producers:
+        print(f'creating {COMMANDS} producer')
+        loop.create_task(command_receiver())
     loop.run_forever()
