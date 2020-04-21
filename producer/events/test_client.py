0f2bfce77ce9eeb4fe6973681280c4ac707c6ed6
José  Galaz <jdgalazm@gmail.com>
2020-04-21 17:14:20 -0400

    add passing test for current evtclient functionality

--- producer/events/test_client.py
+++ producer/events/test_client.py
@@ -19,12 +19,51 @@ class TestEventsClient(test_utils.WSClientTestCase):
             self.csc = salobj.TestCsc(index=self.index, config_dir=None, initial_state=salobj.State.ENABLED)
             self.remote = salobj.Remote(domain=self.csc.domain, name="Test", index=self.index)
 
-            self.client = EventsWSClient(csc_list=[])
+            self.client = EventsWSClient(csc_list=[("Test", self.index)])
             self.client_task = asyncio.create_task(self.client.start_ws_client())
         
         async def act_assert(websocket, path):
-            await asyncio.sleep(5)
-            pass
+            # ARRANGE wait for the client to connect to initial_state group
+            # this ensures the harness will run the arrange() first
+            initial_subscription = await websocket.recv()
+            self.assertEqual(json.loads(initial_subscription), {
+                'option': 'subscribe',
+                'category': 'initial_state',
+                'csc': 'all',
+                'salindex': 'all',
+                'stream': 'all'})
+
+            # ARRANGE: fill in some data            
+            cmd_data_sent = self.csc.make_random_cmd_scalars()
+            await self.remote.cmd_setScalars.start(cmd_data_sent, timeout=STD_TIMEOUT)
+
+            # wait for the scalars event to be sent
+            while True:
+                response = await websocket.recv()
+                message = json.loads(response)
+                from pprint import pprint
+                pprint(message)
+                stream_exists = utils.check_event_stream(message, 'event', 'Test', self.index, 'scalars')
+                if stream_exists:
+                    stream = utils.get_event_stream(message, 'event', 'Test', self.index, 'scalars')[0]
+                    break
+
+            del stream['private_rcvStamp']
+
+            # build expected data
+            evt_scalars = await self.remote.evt_scalars.next(flush=False, timeout=STD_TIMEOUT)
+            evt_parameters = evt_scalars._member_attributes
+
+            expected_stream = {
+                p: {
+                    'value': getattr(evt_scalars, p),
+                    'dataType': utils.getDataType(getattr(evt_scalars, p)),
+                    # 'units': f"{self.remote.evt_scalars.metadata.field_info[p].units}"
+                } for p in evt_parameters if p != "private_rcvStamp"
+            }
+
+            self.assertEqual(stream, expected_stream)
+
         async def cleanup():
             # cleanup
             self.client.retry = False
