3f511e23aa862cd65b08f2890f96ceacb8e79380
Tiago Ribeiro <tiago.astro@gmail.com>
2020-08-25 15:53:46 -0700

    Build love-producer with deployment image. Make build cycle a variable in a .env file.

--- producer/start-daemon.sh
+++ producer/start-daemon.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-. /home/saluser/.setup_dev.sh
+. /home/saluser/.setup_sal_env.sh
 
 export PYTHONPATH=$PYTHONPATH:/usr/src/love/producer
-if [[ $LSST_DDS_IP != *"."* ]]; then
-  echo "Unset LSST_DDS_IP"
-  unset LSST_DDS_IP
-fi
+
 python -u /usr/src/love/producer/main.py
