2764e7aa30b274f3be48aec73fc3e4e5110216ba
tribeiro <tribeiro@lsst.org>
2022-04-04 06:39:22 -0700

    Reformat with black 22.

--- python/love/producer/producer_utils.py
+++ python/love/producer/producer_utils.py
@@ -93,13 +93,13 @@ class NumpyEncoder(json.JSONEncoder):
 
 
 class MissingMessageParameter(Exception):
-    """ Exception class to be raised on missing message parameter """
+    """Exception class to be raised on missing message parameter"""
 
     pass
 
 
 class MissingMessageStream(Exception):
-    """ Exception class to be raised on missing message stream """
+    """Exception class to be raised on missing message stream"""
 
     pass
 
@@ -129,7 +129,7 @@ def get_data_type(value):
 
 
 def onemsg_generator(category, csc, salindex, streams_dict):
-    """Generates one msg for the LOVE-manager from a single (csc,salindex) source """
+    """Generates one msg for the LOVE-manager from a single (csc,salindex) source"""
 
     return {
         "category": category,
