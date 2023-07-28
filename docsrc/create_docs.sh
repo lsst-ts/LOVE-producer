7862240bc3c7335fb296420b0f9dd00426a4ead7
Sebastian Aranda <aranda.sebastian@gmail.com>
2023-07-28 15:21:06 -0400

    Extend create_doc.sh to add .nojekyll and and index.html for redirections from the root

--- docsrc/create_docs.sh
+++ docsrc/create_docs.sh
@@ -5,3 +5,6 @@ python3 edit_apidoc_modules.py
 rm -rf ../docs/doctrees
 rm -rf ../docs/html
 make html
+touch ../docs/.nojekyll
+echo "<meta http-equiv='refresh' content='0; url=./html/index.html' />" > ../docs/index.html
+chown -R 1000:1000 ../docs
