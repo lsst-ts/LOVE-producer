80e413569fe5b49b5c6bf9fb208e9974e9235f58
Sebastian Aranda <aranda.sebastian@gmail.com>
2023-07-28 20:05:15 -0400

    Fix apidoc generation

--- docsrc/create_docs.sh
+++ docsrc/create_docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf source/apidoc
-sphinx-apidoc -o source/apidoc ../producer
-python3 edit_apidoc_modules.py
+sphinx-apidoc -o source/apidoc ../python/love ../python/love/producer/test_utils/*
+python edit_apidoc_modules.py
 rm -rf ../docs/doctrees
 rm -rf ../docs/html
 make html
