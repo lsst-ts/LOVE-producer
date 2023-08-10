6fcde36d8c997d04a7600b7ef7f08517b1050bcc
Sebastian Aranda <aranda.sebastian@gmail.com>
2023-08-09 20:04:46 -0400

    Remove .nojekyll file creation as it is not needed anymore

--- docsrc/create_docs.sh
+++ docsrc/create_docs.sh
@@ -5,5 +5,4 @@ python edit_apidoc_modules.py
 rm -rf ../docs/doctrees
 rm -rf ../docs/html
 make html
-touch ../docs/.nojekyll
 echo "<meta http-equiv='refresh' content='0; url=./html/index.html' />" > ../docs/index.html
